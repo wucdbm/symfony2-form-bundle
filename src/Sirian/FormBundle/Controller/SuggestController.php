2abc0f73fd18b27bc8760b3bee77531783828177
Vasily Khayrulin <sirianru@gmail.com>
2013-10-24 17:27:34 +0400

    improved initSelection

--- src/Sirian/FormBundle/Controller/SuggestController.php
+++ src/Sirian/FormBundle/Controller/SuggestController.php
@@ -3,6 +3,7 @@
 namespace Sirian\FormBundle\Controller;
 
 use Sirian\FormBundle\Suggest\Registry;
+use Sirian\FormBundle\Suggest\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -28,5 +29,28 @@ class SuggestController extends Controller
         $data = $suggester->suggest($query, $options);
         $data->setItems($suggester->transform($data->getItems()));
         return new JsonResponse($data->toArray());
+    }
+
+    public function initAction($name)
+    {
+        /**
+         * @var Registry $registry
+         */
+        $registry = $this->get('sirian_form.suggest_registry');
+        $request = $this->getRequest();
+
+        if (!$registry->hasSuggester($name)) {
+            throw $this->createNotFoundException();
+        }
+
+        $suggester = $registry->getSuggester($name);
+
+        $ids = $request->query->get('ids', '');
+        if (!is_array($ids)) {
+            $ids = explode(',', $ids);
+        }
+        $objects = $suggester->reverseTransform($ids);
+
+        return new JsonResponse($suggester->transform($objects));
     }
 }
